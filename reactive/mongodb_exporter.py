056e9159c552c2af4d533fb847c9d7be65dd598f
Casey Marshall <casey.marshall@canonical.com>
2016-05-24 23:29:05 -0500

    Start if installed and not started.

--- reactive/mongodb_exporter.py
+++ reactive/mongodb_exporter.py
@@ -1,10 +1,16 @@
-from charms.reactive import when, remove_state, hook
+from charms.reactive import when, when_not, set_state, remove_state, hook
 from charmhelpers.core import hookenv
 
 
 @hook('install')
 def install():
     hookenv.open_port(9001)
+
+
+@when('service-workload.available')
+@when_not('service-workload.started')
+def start():
+    set_state('service-workload.start')
 
 
 @when('target.available')
